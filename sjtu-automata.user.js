56034bc1010c649166f08f056d0540543c36a6e7
Xinzhe Wang <me@imwxz.com>
2019-05-31 22:57:54 +0800

    add lesson query #6

--- sjtu-automata.user.js
+++ sjtu-automata.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         sjtu-automata
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  show classid under classname.
 // @author       MXWXZ
 // @match        *://i.sjtu.edu.cn/xsxk/zzxkyzb_cxZzxkYzbIndex.html*
+// @match        *://i.sjtu.edu.cn/design/viewFunc_cxDesignFuncPageIndex.html*
 // @homepageURL  https://github.com/MXWXZ/sjtu-automata/
 // @supportURL   https://github.com/MXWXZ/sjtu-automata/issues/
 // @downloadURL  https://raw.githubusercontent.com/MXWXZ/sjtu-automata/master/sjtu-automata.user.js
@@ -67,15 +68,28 @@ function showid(node) {
     node.children('.jxbmc').append('<p>' + id + '</p>');
 }
 
+function showid2(node) {
+    node.show();
+}
+
 (function () {
     'use strict';
 
-    let node = $('.nav.nav-tabs.sl_nav_tabs li');
-    node.each(function () {
-        let str = $(this).children('a')[0].getAttribute("onclick");
-        let pos1 = str.indexOf("this,'");
-        let pos2 = str.indexOf("','");
-        $(this).append('<p>' + str.substr(str.indexOf("this,'") + 6, pos2 - pos1 - 6) + ' &nbsp;</p>');
-    });
-    waitForKeyElements('.body_tr', showid, false);
+    if (location.href.indexOf("zzxkyzb_cxZzxkYzbIndex") > -1) {
+        let node = $('.nav.nav-tabs.sl_nav_tabs li');
+        node.each(function () {
+            let str = $(this).children('a')[0].getAttribute("onclick");
+            let pos1 = str.indexOf("this,'");
+            let pos2 = str.indexOf("','");
+            $(this).append('<p>' + str.substr(str.indexOf("this,'") + 6, pos2 - pos1 - 6) + ' &nbsp;</p>');
+        });
+        waitForKeyElements('.body_tr', showid, false);
+    } else if(location.href.indexOf("viewFunc_cxDesignFuncPageIndex") > -1) {
+        $('[id$=_jxb_id]').show();
+        $('[id$=_jxb_id]').css('width','270px');
+        let row=$('.jqgfirstrow').children('td:eq(1)');
+        row.show();
+        row.css('width','270px');
+        waitForKeyElements("[aria-describedby$='_jxb_id']", showid2, false);
+    }
 })();
